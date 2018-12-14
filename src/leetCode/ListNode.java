3ee14b86876d2b543fd0435aee3171b2d5491902
xianCan <2492716147@qq.com>
2018-12-14 16:15:23 +0800

    单链表

---
+++ src/leetCode/ListNode.java
@@ -0,0 +1,13 @@
+package leetCode;
+
+/**
+ * @authod xianCan
+ * @date 2018/12/14 9:45
+ *
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
