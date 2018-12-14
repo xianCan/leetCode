3c41c622262078b13e4b0867636367599ea14b0b
xianCan <2492716147@qq.com>
2018-12-14 16:16:50 +0800

    LeetCode第2题：两数相加

---
+++ src/leetCode/LeetCode2.java
@@ -0,0 +1,41 @@
+package leetCode;
+
+import java.util.Stack;
+
+/**
+ * @authod xianCan
+ * @date 2018/12/14 9:49
+ *
+ * 2.两数相加
+ *
+ * 题目描述：
+ *     给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ */
+public class LeetCode2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode p = l1, q = l2,cur=head;
+        int carry=0;
+        while (p != null || q != null) {
+            int x = (p==null)?0:p.val;
+            int y = (q==null)?0:q.val;
+            int temp = x+y+carry;
+            carry=temp / 10;
+            cur.next = new ListNode(temp%10);
+            cur = cur.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0){
+            cur.next = new ListNode(carry);
+        }
+        return head.next;
+    }
+}
