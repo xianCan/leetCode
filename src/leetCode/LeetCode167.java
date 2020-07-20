98f919d7d310397d8beeb96be4153744994f947d
xianCan <2492716147@qq.com>
2020-07-20 21:39:50 +0800

    LeetCode第167题：两数之和 II - 输入有序数组（双指针）

---
+++ src/leetCode/LeetCode167.java
@@ -0,0 +1,47 @@
+package leetCode;
+
+/**
+ * @author xianCan
+ * @date 2020/7/20 21:30
+ *
+ * 167. 两数之和 II - 输入有序数组
+ *
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+
+    函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+
+    说明:
+
+    返回的下标值（index1 和 index2）不是从零开始的。
+    你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+    示例:
+
+    输入: numbers = [2, 7, 11, 15], target = 9
+    输出: [1,2]
+    解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+
+ */
+public class LeetCode167 {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left=0, right=numbers.length-1;
+        while (left < right){
+            int tmp = numbers[left] + numbers[right];
+            if (tmp == target){
+                return new int[]{left + 1, right + 1};
+            } else if (tmp < target){
+                left++;
+            } else if (tmp > target){
+                right--;
+            }
+        }
+        return new int[]{left + 1, right + 1};
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new LeetCode167().twoSum(new int[]{2, 7, 11, 15}, 9);
+        for (int i : ints){
+            System.out.println(i);
+        }
+    }
+}
