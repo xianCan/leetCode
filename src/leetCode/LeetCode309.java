34d3a0261e1939481bb2b7944a95ed514d7e9373
xianCan <2492716147@qq.com>
2020-07-24 21:24:51 +0800

    LeetCode第309题：最佳买卖股票时机含冷冻期（动态规划）

---
+++ src/leetCode/LeetCode309.java
@@ -0,0 +1,49 @@
+package leetCode;
+
+/**
+ * @author xianCan
+ * @date 2020/7/24 20:35
+ *
+ * 309. 最佳买卖股票时机含冷冻期
+ *
+ * 给定一个整数数组，其中第 i 个元素代表了第 i 天的股票价格 。​
+
+    设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+
+    你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+    卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+    示例:
+
+    输入: [1,2,3,0,2]
+    输出: 3
+    解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+
+ */
+public class LeetCode309 {
+
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length==0 || prices.length == 1) return 0;
+        int[][] dp = new int[prices.length][2];
+
+        int length = prices.length;
+        for (int i=0; i<length; i++){
+            if (i==0){
+                dp[0][0] = 0;
+                dp[0][1] = - prices[0];
+                continue;
+            } else if (i==1){
+                dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i-1][1], - prices[i]);
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-2][0] - prices[i]);
+        }
+        return dp[length-1][0];
+    }
+
+    public static void main(String[] args) {
+        int i = new LeetCode309().maxProfit(new int[]{1, 2, 4});
+        System.out.println(i);
+    }
+}
