b4b5ae868e48f723931a926c157e557b215b51a0
xianCan <2492716147@qq.com>
2020-08-01 13:13:46 +0800

    LeetCode第213题：打家劫舍 II（中等）（动态规划）

---
+++ src/leetCode/LeetCode213.java
@@ -0,0 +1,57 @@
+package leetCode;
+
+/**
+ * @author xianCan
+ * @date 2020/8/1 11:09
+ *
+ * 213. 打家劫舍 II
+ *
+ *  你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都围成一圈，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+
+    给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+
+    示例 1:
+
+    输入: [2,3,2]
+    输出: 3
+    解释: 你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+    示例 2:
+
+    输入: [1,2,3,1]
+    输出: 4
+    解释: 你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+         偷窃到的最高金额 = 1 + 3 = 4 。
+
+ */
+public class LeetCode213 {
+    /**
+     * 分三种情况：
+     * 1、偷第一个不偷最后一个
+     * 2、不偷第一个偷最后一个
+     * 3、首尾两个都不偷
+     * 所以解决方案就是划定一下区间就行
+     * @param nums
+     * @return
+     */
+    public int rob(int[] nums) {
+        if (nums==null||nums.length==0) return 0;
+        if (nums.length == 1) return nums[0];
+        int length = nums.length;
+        return Math.max(robRange(nums, 0, length-2), robRange(nums, 1, length-1));
+    }
+
+    private int robRange(int[] nums, int start, int end){
+        int dp_i_1=0, dp_i_2=0, dp_i=0;
+        for (int i=start; i<= end; i++){
+            dp_i = Math.max(nums[i] + dp_i_2, dp_i_1);
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
+        }
+        return dp_i;
+    }
+
+    public static void main(String[] args) {
+        int rob = new LeetCode213().rob(new int[]{2, 3, 2});
+        System.out.println(rob);
+    }
+}
