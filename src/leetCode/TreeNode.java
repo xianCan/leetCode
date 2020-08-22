430cf2ff5bd34b7f02b09f145a194346bf45c2c7
xianCan <2492716147@qq.com>
2020-08-22 17:21:50 +0800

    剑指Offer第68题：二叉搜索树的最近公共祖先（简单）（利用二叉搜索树特性进行迭代）

--- src/leetCode/TreeNode.java
+++ src/leetCode/TreeNode.java
@@ -6,9 +6,9 @@ package leetCode;
  */
 public class TreeNode {
 
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
     TreeNode() {}
     TreeNode(int val) { this.val = val; }
     TreeNode(int val, TreeNode left, TreeNode right) {
