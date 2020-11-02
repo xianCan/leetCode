0c63604b2f636bebcae68f2a92e2fbc32ab1067d
xianCan <2492716147@qq.com>
2020-11-02 20:32:57 +0800

    LeetCode第349题：两个数组的交集（简单）（哈希表）

---
+++ src/leetCode/LeetCode349.java
@@ -0,0 +1,57 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author xianCan
+ * @date 2020/11/2 20:24
+ *
+ * 349. 两个数组的交集（简单）
+ *
+ *  给定两个数组，编写一个函数来计算它们的交集。
+
+    示例 1：
+
+    输入：nums1 = [1,2,2,1], nums2 = [2,2]
+    输出：[2]
+    示例 2：
+
+    输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+    输出：[9,4]
+     
+
+    说明：
+
+    输出结果中的每个元素一定是唯一的。
+    我们可以不考虑输出结果的顺序。
+
+ */
+public class LeetCode349 {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int i: nums1){
+            set1.add(i);
+        }
+
+        Set<Integer> set2 = new HashSet<>();
+        for (int i: nums2){
+            set2.add(i);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (Integer i : set1){
+            if (set2.contains(i)){
+                list.add(i);
+            }
+        }
+        int[] ans = new int[list.size()];
+        for (int i=0; i<list.size(); i++){
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+}
