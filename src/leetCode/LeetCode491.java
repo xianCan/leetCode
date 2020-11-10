5671a2dba8cb5b5af44bcba693f41fc03ad858d9
xianCan <2492716147@qq.com>
2020-11-10 20:44:19 +0800

    LeetCode第491题：递增子序列（中等）（回溯+剪枝）

--- src/leetCode/LeetCode491.java
+++ src/leetCode/LeetCode491.java
@@ -1,8 +1,6 @@
 package leetCode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author xianCan
@@ -26,8 +24,7 @@ import java.util.List;
 public class LeetCode491 {
 
     private int[] nums;
-    private List<List<Integer>> res = new ArrayList<>();
-    private LinkedList<Integer> temp = new LinkedList<>();
+    private List<List<Integer>> res;
 
     /**
      * 暴力回溯
@@ -36,31 +33,32 @@ public class LeetCode491 {
      */
     public List<List<Integer>> findSubsequences(int[] nums) {
         this.nums = nums;
-        recursion(0, Integer.MIN_VALUE);
+        this.res = new ArrayList<>();
+        recursion(0, new LinkedList<>());
         return res;
     }
 
-    private void recursion(int cur, int last){
-        if (cur == nums.length){
-            if (temp.size() > 1){
-                res.add(new LinkedList<>(temp));
-            }
-            return;
+    private void recursion(int start, LinkedList<Integer> path){
+        if (path.size() > 1){
+            res.add(new ArrayList<>(path));
         }
 
-        if (nums[cur] >= last){
-            temp.add(nums[cur]);
-            recursion(cur + 1, nums[cur]);
-            temp.removeLast();
-        }
-        if (nums[cur] != last){
-            recursion(cur + 1, last);
+        //由于不能排序，因此以前的排序，通过一个标记数组来达到去重是不可用的
+
+        //同层上使用过的元素不能再使用了，在同一层上通过一个Set来标记用过的元素
+        Set<Integer> set = new HashSet<>();
+        for (int i=start; i<nums.length; i++){
+            if ((path.isEmpty() || nums[i] >= path.getLast()) && !set.contains(nums[i])){
+                set.add(nums[i]);
+                path.add(nums[i]);
+                recursion(i+1, path);
+                path.removeLast();
+            }
         }
     }
 
     public static void main(String[] args) {
-        List<List<Integer>> subsequences = new LeetCode491().findSubsequences(new int[]{1,2,3,4,5,6,7,8,9,10,1,1,1,1,1});
-
+        List<List<Integer>> subsequences = new LeetCode491().findSubsequences(new int[]{7,4,6,7,7});
         System.out.println(subsequences);
     }
 }
