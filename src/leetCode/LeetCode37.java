fdf3f99a745fb550e9fa21ff2b9af4b2a04b3d19
xianCan <2492716147@qq.com>
2020-11-22 00:22:29 +0800

    LeetCode第37题：解数独（困难）（回溯）

--- src/leetCode/LeetCode37.java
+++ src/leetCode/LeetCode37.java
@@ -1,8 +1,5 @@
 package leetCode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author xianCan
  * @date 2020/9/15 22:32
@@ -35,42 +32,49 @@ import java.util.List;
  */
 public class LeetCode37 {
 
-    private boolean[][] row = new boolean[9][9];
-    private boolean[][] column = new boolean[9][9];
-    private boolean[][][] block = new boolean[3][3][9];
-    private boolean valid = false;
-    private List<int[]> spaces = new ArrayList<>();
-
     public void solveSudoku(char[][] board) {
-        for (int i=0; i<9; i++){
-            for (int j=0; j<9; j++){
-                if (board[i][j] == '.'){
-                    spaces.add(new int[]{i, j});
-                } else {
-                    int num = board[i][j] - '0' - 1;
-                    row[i][num] = column[j][num] = block[i/3][j/3][num] = true;
+        backtracking(board);
+    }
+
+    private boolean backtracking(char[][] board){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[0].length; j++){
+                if (board[i][j] != '.') continue;
+                for (char k = '1'; k <= '9'; k++){
+                    if (isValid(i, j, k, board)){
+                        board[i][j] = k;
+                        if (backtracking(board)) return true;
+                        board[i][j] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(int row, int col, char val, char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == val) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i++){
+            if (board[i][col] == val){
+                return false;
+            }
+        }
+
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == val) {
+                    return false;
                 }
             }
         }
-        helper(board, 0);
-    }
-
-    private void helper(char[][] board, int start){
-        if (start == spaces.size()){
-            valid = true;
-            return;
-        }
-
-        int[] space = spaces.get(start);
-        int i=space[0], j=space[1];
-
-        for (int num=0; num<9 && !valid; num++){
-            if (!row[i][num] && !column[j][num] && !block[i/3][j/3][num]){
-                row[i][num] = column[j][num] = block[i/3][j/3][num] = true;
-                board[i][j] = (char) (num + '0' + 1);
-                helper(board, start+1);
-                row[i][num] = column[j][num] = block[i/3][j/3][num] = false;
-            }
-        }
+        return true;
     }
 }
