4cb55ac1939f65a0d5164426737dbef0abbdb8d5
xianCan <2492716147@qq.com>
2020-11-28 21:50:53 +0800

    LeetCode第53题：最大子序和（简单）（动态规划、贪心）

--- src/leetCode/LeetCode53.java
+++ src/leetCode/LeetCode53.java
@@ -44,8 +44,28 @@ public class LeetCode53 {
         return res;
     }
 
+    /**
+     * 贪心：加上当前数字，如果小于0，则丢掉开始，重新赋0
+     * @param nums
+     * @return
+     */
+    public int maxSubArray2(int[] nums){
+        if (nums==null || nums.length==0) return 0;
+        int ans=Integer.MIN_VALUE, sum=0;
+        for (int num : nums){
+            sum += num;
+            if (sum > ans){
+                ans = sum;
+            }
+            if (sum < 0){
+                sum = 0;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int i = new LeetCode53().maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+        int i = new LeetCode53().maxSubArray2(new int[]{-2,1,-3,4,-1,2,1,-5,4});
         System.out.println(i);
     }
 }
