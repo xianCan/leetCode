64f6a5030899c58f981bfb6bd7cd911e205690a9
xianCan <2492716147@qq.com>
2021-01-01 00:21:50 +0800

    LeetCode第605题：种花问题（简单）（迭代）

---
+++ src/leetCode/LeetCode605.java
@@ -0,0 +1,58 @@
+package leetCode;
+
+/**
+ * @author xianCan
+ * @date 2021/1/1 0:06
+ *
+ * 605. 种花问题（简单）
+ *
+ *  假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+
+    给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
+
+    示例 1:
+
+    输入: flowerbed = [1,0,0,0,1], n = 1
+    输出: True
+    示例 2:
+
+    输入: flowerbed = [1,0,0,0,1], n = 2
+    输出: False
+    注意:
+
+    数组内已种好的花不会违反种植规则。
+    输入的数组长度范围为 [1, 20000]。
+    n 是非负整数，且不会超过输入数组的大小。
+
+ */
+public class LeetCode605 {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int ans = 0, len = 0;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (i == 0 && flowerbed[i] == 0){ // 起始
+                len++;
+            } else if (flowerbed[i] == 0 && i > 0 && flowerbed[i - 1] != 1){ // 中间
+                len++;
+            } else if (flowerbed[i] == 1){
+                len -= 1;
+                if (len > 0){
+                    ans += (len + 1) / 2;
+                }
+                len = 0;
+            }
+        }
+
+        if (len > 0){
+            ans += (len + 1) / 2;
+        }
+
+        return ans >= n;
+    }
+
+    public static void main(String[] args) {
+        boolean b = new LeetCode605().canPlaceFlowers(new int[]{1, 0, 0, 0, 0}, 2);
+        System.out.println(b);
+    }
+}
