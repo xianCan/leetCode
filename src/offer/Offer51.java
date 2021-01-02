9455e5950097d7681f0185958b70504fddd1179a
xianCan <2492716147@qq.com>
2021-01-02 22:56:54 +0800

    剑指Offer第51题：逆序对（困难）（归并排序）

--- src/offer/Offer51.java
+++ src/offer/Offer51.java
@@ -21,10 +21,55 @@ package offer;
  */
 public class Offer51 {
 
+    /**
+     * 归并排序，O(N log N)
+     * @param nums
+     * @return
+     */
     public int reversePairs(int[] nums) {
         if (nums == null || nums.length < 2){
             return 0;
         }
-        return 0;
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private int helper(int[] nums, int left, int right){
+        if (left == right){
+            return 0;
+        }
+
+        int mid = (left + right) / 2;
+        int res1 = helper(nums, left, mid);
+        int res2 = helper(nums, mid + 1, right);
+        int res = res1 + res2;
+
+        int[] sortNum = new int[right - left + 1];
+        int i = left, j = mid + 1, idx = 0;
+        while (i <= mid || j <= right){
+            if (i > mid){
+                //下标已越过第一个数组右边界，直接取第二个数组
+                sortNum[idx++] = nums[j++];
+            } else if (j > right){
+                //下标已越过第二个数组右边界，直接取第二个数组
+                sortNum[idx++] = nums[i++];
+            } else if (nums[j] < nums[i]){
+                //nums[j] < nums[i]，则 nums[j] 小于 nums[i] 的所有数，此时进行逆序对的统计
+                sortNum[idx++] = nums[j++];
+                res += mid - i + 1;
+            } else {
+                sortNum[idx++] = nums[i++];
+            }
+        }
+
+        for (int k = 0; k < right - left + 1; k++){
+            nums[k + left] = sortNum[k];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int i = new Offer51().reversePairs(new int[]{5});
+        System.out.println(i);
     }
 }
