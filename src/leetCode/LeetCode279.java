94ffcdfa0f52ab131b2817004c3f2569942288e9
xianCan <2492716147@qq.com>
2021-02-04 22:23:53 +0800

    LeetCode第279题：完全平方数（中等）（动态规划：完全背包）

---
+++ src/leetCode/LeetCode279.java
@@ -0,0 +1,69 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xianCan
+ * @date 2021/2/4 21:58
+ *
+ * 279. 完全平方数（中等）
+ *
+ *  给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+
+    给你一个整数 n ，返回和为 n 的完全平方数的 最少数量 。
+
+    完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+
+     
+
+    示例 1：
+
+    输入：n = 12
+    输出：3
+    解释：12 = 4 + 4 + 4
+    示例 2：
+
+    输入：n = 13
+    输出：2
+    解释：13 = 4 + 9
+     
+    提示：
+
+    1 <= n <= 10^4
+
+ */
+public class LeetCode279 {
+
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++){
+            dp[i] = i;
+        }
+
+        List<Integer> powList = new ArrayList<>();
+        for (int i = 1;;i++){
+            int pow = (int) Math.pow(i, 2);
+            if (pow > n){
+                break;
+            }
+            powList.add(pow);
+        }
+
+        for (int pow : powList){
+            for (int j = 1; j <= n; j++){
+                if (j - pow >= 0){
+                    dp[j] = Math.min(dp[j], dp[j - pow] + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int i = new LeetCode279().numSquares(10000);
+        System.out.println(i);
+    }
+}
