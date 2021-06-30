cd2448df6b59eef5c80444ac63dafe9d8572349c
xianCan <2492716147@qq.com>
2021-06-30 20:05:55 +0800

    剑指Offer第37题：序列化二叉树（困难）（dfs和bfs）

---
+++ src/offer/Offer37.java
@@ -0,0 +1,66 @@
+package offer;
+
+import leetCode.TreeNode;
+
+/**
+ * @author xianCan
+ * @date 2021/6/30 19:55
+ *
+ * 剑指 Offer 37. 序列化二叉树（困难）
+ *
+ *  请实现两个函数，分别用来序列化和反序列化二叉树。
+
+    你需要设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+
+    提示：输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+
+     
+
+    示例：
+
+
+    输入：root = [1,2,3,null,null,4,5]
+    输出：[1,2,3,null,null,4,5]
+
+ */
+public class Offer37 {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder builder = new StringBuilder();
+        dfs(root, builder);
+        return builder.toString();
+    }
+
+    private void dfs(TreeNode node, StringBuilder builder){
+        if (node == null){
+            builder.append("#").append(",");
+            return;
+        }
+        builder.append(node.val).append(",");
+        dfs(node.left, builder);
+        dfs(node.right, builder);
+    }
+
+    private int start = 0;
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] strs = data.split(",");
+        start = 0;
+        return dfs(strs);
+    }
+
+    private TreeNode dfs(String[] strs){
+        if ("#".equals(strs[start]) || start >= strs.length){
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.parseInt(strs[start]));
+        ++start;
+        node.left = dfs(strs);
+        ++start;
+        node.right = dfs(strs);
+        return node;
+    }
+}
