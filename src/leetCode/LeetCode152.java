8a3df815ec5c489d88b8f389161d9e8e0298a963
xianCan <2492716147@qq.com>
2021-11-23 21:47:40 +0800

    LeetCode第152题：乘积最大子数组（中等）（动态规划）

---
+++ src/leetCode/LeetCode152.java
@@ -0,0 +1,47 @@
+package leetCode;
+
+/**
+ * @author xianCan
+ * @date 2021/11/23 21:35
+ *
+ * 152. 乘积最大子数组（中等）
+ *
+ *  给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+
+    示例 1:
+
+    输入: [2,3,-2,4]
+    输出: 6
+    解释: 子数组 [2,3] 有最大乘积 6。
+    示例 2:
+
+    输入: [-2,0,-1]
+    输出: 0
+    解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+
+ */
+public class LeetCode152 {
+
+    public int maxProduct(int[] nums) {
+        int length = nums.length;
+        int[] maxF = new int[length];
+        int[] minF = new int[length];
+        System.arraycopy(nums, 0, maxF, 0, length);
+        System.arraycopy(nums, 0, minF, 0, length);
+        for (int i = 1; i < length; ++i) {
+            maxF[i] = Math.max(maxF[i - 1] * nums[i], Math.max(nums[i], minF[i - 1] * nums[i]));
+            minF[i] = Math.min(minF[i - 1] * nums[i], Math.min(nums[i], maxF[i - 1] * nums[i]));
+        }
+        int ans = maxF[0];
+        for (int i = 1; i < length; ++i) {
+            ans = Math.max(ans, maxF[i]);
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int i = new LeetCode152().maxProduct(new int[]{2,-5,-2,-4,3});
+        System.out.println(i);
+    }
+}
