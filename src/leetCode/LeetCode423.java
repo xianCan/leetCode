6375d8dc8711f93ed6c60696a36feb7d28581c9e
xianCan <2492716147@qq.com>
2021-11-24 20:05:15 +0800

    LeetCode第423题：从英文中重建数字（中等）（字符统计）

---
+++ src/leetCode/LeetCode423.java
@@ -0,0 +1,63 @@
+package leetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author xianCan
+ * @date 2021/11/24 19:58
+ *
+ * 423. 从英文中重建数字（中等）
+ *
+ *  给你一个字符串 s ，其中包含字母顺序打乱的用英文单词表示的若干数字（0-9）。按 升序 返回原始的数字。
+
+    示例 1：
+
+    输入：s = "owoztneoer"
+    输出："012"
+    示例 2：
+
+    输入：s = "fviefuro"
+    输出："45"
+     
+
+    提示：
+
+    1 <= s.length <= 105
+    s[i] 为 ["e","g","f","i","h","o","n","s","r","u","t","w","v","x","z"] 这些字符之一
+    s 保证是一个符合题目要求的字符串
+
+ */
+public class LeetCode423 {
+
+    public String originalDigits(String s) {
+        Map<Character, Integer> c = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            c.put(ch, c.getOrDefault(ch, 0) + 1);
+        }
+
+        int[] cnt = new int[10];
+        cnt[0] = c.getOrDefault('z', 0);
+        cnt[2] = c.getOrDefault('w', 0);
+        cnt[4] = c.getOrDefault('u', 0);
+        cnt[6] = c.getOrDefault('x', 0);
+        cnt[8] = c.getOrDefault('g', 0);
+
+        cnt[3] = c.getOrDefault('h', 0) - cnt[8];
+        cnt[5] = c.getOrDefault('f', 0) - cnt[4];
+        cnt[7] = c.getOrDefault('s', 0) - cnt[6];
+
+        cnt[1] = c.getOrDefault('o', 0) - cnt[0] - cnt[2] - cnt[4];
+
+        cnt[9] = c.getOrDefault('i', 0) - cnt[5] - cnt[6] - cnt[8];
+
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < cnt[i]; ++j) {
+                ans.append((char) (i + '0'));
+            }
+        }
+        return ans.toString();
+    }
+}
