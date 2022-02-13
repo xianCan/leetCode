88248ddd66331a67f9eb4a2740d40ac1ed7f86af
xianCan <2492716147@qq.com>
2022-02-13 15:52:20 +0800

    LeetCode第28题：实现 strStr()（简单）（KMP）

--- src/leetCode/LeetCode28.java
+++ src/leetCode/LeetCode28.java
@@ -72,31 +72,43 @@ public class LeetCode28 {
      * @return
      */
     public int strStr2(String haystack, String needle) {
-        int n = haystack.length(), m = needle.length();
-        if (m == 0) {
+        if (needle.length() == 0){
             return 0;
         }
-        int[] pi = new int[m];
-        for (int i = 1, j = 0; i < m; i++) {
-            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
-                j = pi[j - 1];
+        int[] prefix = getPrefix(needle);
+        //j代表needle已匹配长度
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++){
+            if (j > 0 && haystack.charAt(i) != needle.charAt(j)){
+                j = prefix[j - 1];
             }
-            if (needle.charAt(i) == needle.charAt(j)) {
+            if (haystack.charAt(i) == needle.charAt(j)){
                 j++;
             }
-            pi[i] = j;
-        }
-        for (int i = 0, j = 0; i < n; i++) {
-            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
-                j = pi[j - 1];
-            }
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                j++;
-            }
-            if (j == m) {
-                return i - m + 1;
+            if (j == needle.length()){
+                return i - j + 1;
             }
         }
         return -1;
+    }
+
+    private int[] getPrefix(String needle){
+        int[] prefix = new int[needle.length()];
+        prefix[0] = 0;
+        //j代表前缀的长度
+        int j = 0;
+
+        //i代表后缀的长度
+        for (int i = 1; i < needle.length(); i++){
+            while (j > 0 && needle.charAt(i) != needle.charAt(j)){
+                j = prefix[j - 1];
+            }
+            if (needle.charAt(i) == needle.charAt(j)){
+                j++;
+            }
+            prefix[i] = j;
+        }
+
+        return prefix;
     }
 }
