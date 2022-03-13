698c00af9cfcb85f4914c6557a2d695bc5e89258
xianCan <2492716147@qq.com>
2022-03-13 20:25:20 +0800

    LeetCode第162题：寻找峰值（中等）（二分）

--- src/leetCode/LeetCode162.java
+++ src/leetCode/LeetCode162.java
@@ -59,4 +59,25 @@ public class LeetCode162 {
 
         return -1;
     }
+
+    /**
+     * 首先要注意题目条件，在题目描述中出现了 nums[-1] = nums[n] = -∞，并且对于所有有效的 i 都有 nums[i] != nums[i + 1]
+     * 这就代表着 只要数组中存在一个元素比相邻元素大，那么沿着它一定可以找到一个峰值
+     *
+     * 根据上述结论，我们就可以使用二分查找找到峰值查找时，左指针 l，右指针 r，以其保持左右顺序为循环条件
+     * 根据左右指针计算中间位置 m，并比较 m 与 m+1 的值，如果 m 较大，则左侧存在峰值，r = m，如果 m + 1 较大，则右侧存在峰值，l = m + 1
+     */
+    public int findPeakElement2(int[] nums){
+        int l = 0, r = nums.length - 1;
+        while (l < r){
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < nums[mid + 1]){
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l;
+    }
 }
